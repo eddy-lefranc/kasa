97be15113fcdc2e204f0d8629f0d764698663c80
Eddy Lefranc <lefranc-eddy@protonmail.com>
2023-04-18 19:20:43 +0200

    Amélioration composant 'Header'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,39 @@
-import Navigation from './Navigation';
+// Importations
+
+import { Link } from 'react-router-dom';
+import LOGO from '../images/logos/LOGO.png';
+import styles from '../styles/components/header.module.css';
+
+// Définition de la fonction "Header"
 
 function Header() {
   return (
     <header>
-      <Navigation />
+      <nav className={styles.flexboxSpaceBetween}>
+        <img src={LOGO} alt="logo-kasa" />
+        <ul className={` ${styles.flexboxSpaceBetween} ${styles.header__nav__ul}`}>
+          <li>
+            <Link
+              to="/"
+              className={`${styles.deleteTextDecoration} ${styles.colorFF6060}`}
+            >
+              Accueil
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={`${styles.deleteTextDecoration} ${styles.colorFF6060}`}
+            >
+              A propos
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
+
+// Exportation de la fonction "Header"
 
 export default Header;

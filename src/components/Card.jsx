1321530d1a9b0629ffad8cd293c5bf3397665390
Eddy Lefranc <lefranc-eddy@protonmail.com>
2023-04-20 18:25:05 +0200

    Composant 'Card'

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+// Importations
+
+import styles from '../styles/components/card.module.css';
+import data from '../data/logements.json';
+import { Link } from 'react-router-dom';
+
+// Définition de la fonction "Card"
+
+function Card() {
+  return data.map((housing) => (
+    <Link to={`/housings/${housing.id}`} key={housing.id} className={styles.main__section__a}>
+      <article>
+        <img src={housing.cover} alt={housing.title} />
+        <div>
+          <p>{housing.title}</p>
+        </div>
+      </article>
+    </Link >
+  ));
+}
+
+// Exportation de la fonction "Card"
+
+export default Card;

780ab2e1a1a1f8145b93f5730595637f8c5ff561
Eddy Lefranc <lefranc-eddy@protonmail.com>
2023-05-08 11:37:50 +0200

    Finalisation du composant 'Tag'

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,17 +1,23 @@
-import styles from '../styles/components/tag.module.css'
+// Importation du style pour le composant "Tag"
+
+import styles from '../styles/components/tag.module.css';
+
+// Définition de la fonction "Tag"
 
 function Tag({ tagData }) {
-
     return (
         <div className={styles.main__tags__container}>
             {tagData.map((tag, index) => {
-                return <div key={index} className={styles.main__tag__container}>
-                    <p>{tag}</p>
-                </div>;
+                return (
+                    <div key={index} className={styles.main__tag__container}>
+                        <p>{tag}</p>
+                    </div>
+                );
             })}
         </div>
-    )
-
+    );
 }
+
+// Exportation de la fonction "Tag"
 
 export default Tag;

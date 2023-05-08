33100549078f255807b87f0ff7e8da9f1e46f58c
Eddy Lefranc <lefranc-eddy@protonmail.com>
2023-05-08 11:12:35 +0200

    Finalisation composant 'Collapse'

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,45 +1,25 @@
 // Importations
 
-import styles from '../styles/components/collapse.module.css';
 import React, { useState } from 'react';
-import data from '../data/AboutContent.json';
-import vectorUp from '../images/vectorUp.png';
-import vectorDown from '../images/vectorDown.png';
+import CollapseSection from './CollapseSection';
 
 // Définition de la fonction "Collapse" 
 
-function Collapse() {
-
-    const [clicked, setClicked] = useState(false)
-    const toggle = index => {
-        if (clicked === index) {
-            return setClicked(null)
-        } else {
-            setClicked(index)
-        }
-    }
-
+function Collapse({ sections }) {
+    const [activeIndex, setActiveIndex] = useState(false);
     return (
-        <div className={styles.main__collapse}>
-            <div className={styles.main__collapse__container}>
-                {data.map((item, index) => {
-                    return (
-                        <React.Fragment key={index}>
-                            <div className={styles.main__collapse__wrap} onClick={() => toggle(index)} key={index}>
-                                <h1>{item.Title}</h1>
-                                <span>{clicked === index ? <img src={vectorUp} alt='show-icon' /> : <img src={vectorDown} alt='hide-icon' />}</span>
-                            </div>
-                            {clicked === index ? (
-                                <div className={styles.main__collapse__dropdown}>
-                                    <p className={styles.main__collapse__text}>{item.Content}</p>
-                                </div>
-                            ) : null}
-                        </React.Fragment>
-                    )
-                })}
-            </div>
-        </div>
-    )
+        <React.Fragment>
+            {sections.map((section, index) => (
+                <CollapseSection
+                    section={section}
+                    key={index}
+                    isActiveSection={index === activeIndex}
+                    setActiveIndex={setActiveIndex}
+                    sectionIndex={index}
+                />
+            ))}
+        </React.Fragment>
+    );
 }
 
 // Exportation de la fonction "Collapse"

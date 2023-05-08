49c06fa2044eec75bfac55eb13da5993481dc10c
Eddy Lefranc <lefranc-eddy@protonmail.com>
2023-05-08 11:14:57 +0200

    Finalisation du composant 'Footer'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,19 @@
 // Importations
 
-import logoFooter from '../images/logo/LogoFooter.png'
-import styles from '../styles/components/footer.module.css'
+import styles from '../styles/components/footer.module.css';
+import logo from '../images/logos/logo-footer.png';
 
 // Définition de la fonction "Footer"
 
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <img src={logoFooter} alt='logo-footer-kasa' width={122} />
+            <img src={logo} alt='logo-footer-kasa' width={122} />
             <p className={styles.footer__p}>© 2020 Kasa. All rights reserved</p>
         </footer >
-    )
+    );
 }
 
 // Exportation de la fonction "Footer"
 
-export default Footer
+export default Footer;

52f38be47e35cfe4a9069f45c462ce45114ce886
Eddy Lefranc <lefranc-eddy@protonmail.com>
2023-05-12 14:37:41 +0200

    Ajout d'une condition pour vérifier les types de collapses à afficher

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -5,21 +5,46 @@ import CollapseSection from './CollapseSection';
 
 // Définition de la fonction "Collapse" 
 
-function Collapse({ sections }) {
+function Collapse({ page, sections }) {
+
     const [activeIndex, setActiveIndex] = useState(false);
-    return (
-        <React.Fragment>
-            {sections.map((section, index) => (
-                <CollapseSection
-                    section={section}
-                    key={index}
-                    isActiveSection={index === activeIndex}
-                    setActiveIndex={setActiveIndex}
-                    sectionIndex={index}
-                />
-            ))}
-        </React.Fragment>
-    );
+
+    if (page === "about") {
+        return ( // Collapse page "About"
+            <React.Fragment>
+                {sections.map((section, index) => (
+                    <CollapseSection
+                        section={section}
+                        key={index}
+                        isActiveSection={index === activeIndex}
+                        setActiveIndex={setActiveIndex}
+                        sectionIndex={index}
+                        collapseContainerClass="main__collapse__container"
+                        collapseHeadingClass="main__collapse__about__h1"
+                        collapseDropdownClass="main__collapse__dropdown"
+                    />
+                ))}
+            </React.Fragment>
+        );
+    } else {
+        return ( // Collapse page "Housing"
+            <React.Fragment>
+                {sections.map((section, index) => (
+                    <CollapseSection
+                        section={section}
+                        key={index}
+                        isActiveSection={index === activeIndex}
+                        setActiveIndex={setActiveIndex}
+                        sectionIndex={index}
+                        collapseContainerClass="main__collapse__container__housing"
+                        collapseHeadingClass="main__collapse__housing__h1"
+                        collapseDropdownClass="main__collapse__dropdown__housing"
+                    />
+                ))}
+            </React.Fragment>
+        );
+    }
+
 }
 
 // Exportation de la fonction "Collapse"
